52b27d438dae952ebff35ac366c316b879a7efe1
samueldass96 <sdass@unomaha.edu>
2022-03-02 14:21:48 -0600

    updated

--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'carviewer.apps.CarviewerConfig',
-    'crispy_forms',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
