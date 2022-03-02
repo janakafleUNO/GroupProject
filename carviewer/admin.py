84dab2a10c8d7545acb9016aff609d2d4c8e3a0e
AnuragPanithi <apanithi@unomaha.edu>
2022-03-02 15:21:25 -0600

    models added Car_Type and Car_Name

--- carviewer/admin.py
+++ carviewer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Car_Type, Car_Brand
 
-# Register your models here.
+
+# Define the admin options for the Customer table
+class Type(admin.ModelAdmin):
+    list_display = ('Car_BodyType', 'Car_FuelType', 'Car_Transmission')
+    list_filter = ('Car_BodyType', 'Car_FuelType')
+    search_fields = ('Car_BodyType', 'Car_FuelType', 'Car_Transmission')
+
+class Name(admin.ModelAdmin):
+    list_display = ('Car_Brand', 'Car_Year')
+
+
+admin.site.register(Car_Type, Type)
+admin.site.register(Car_Brand, Name)
--- carviewer/models.py
+++ carviewer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#Define the model for the Customer table
+class Car_Brand(models.Model):
+    Car_Brand = models.CharField(max_length=50)
+    Car_Year = models.CharField(max_length=50)
+
+    def __str__ (self):
+        return self.Car_Brand
+class Car_Type(models.Model):
+    Car_BodyType = models.CharField(max_length=50)
+    Car_FuelType = models.CharField(max_length=50)
+    Car_Transmission = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Car_Type
