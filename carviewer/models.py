39219b4fc205aec86ed9b82458076a8d71e6c25e
AnuragPanithi <apanithi@unomaha.edu>
2022-03-03 15:36:55 -0600

    Test View, Form, Model, HTMLs

--- carviewer/models.py
+++ carviewer/models.py
@@ -3,26 +3,45 @@ from django.utils import timezone
 
 #Define the model for the Customer table
 class Car_Brand(models.Model):
-    Car_Brand = models.CharField(max_length=50)
-    Car_Year = models.CharField(max_length=50)
+    Car_Brand = models.CharField(max_length=50, db_index=True)
+    Car_Year = models.IntegerField(max_length=50, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Car_Brand'
+        verbose_name_plural = 'Car_Brands'
 
     def __str__ (self):
         return self.Car_Brand
+
+
 class Car_Type(models.Model):
+    Category = models.ForeignKey(Car_Brand, related_name='products', on_delete=models.CASCADE)
     Car_BodyType = models.CharField(max_length=50)
     Car_FuelType = models.CharField(max_length=50)
     Car_Transmission = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
 
     def __str__(self):
         return self.Car_Type
+
+
 class Car_Model(models.Model):
     Model_Name = models.CharField(max_length=50)
-    Car_Year = models.CharField(max_length=50)
+    Car_Year = models.IntegerField(max_length=50)
     Car_Prize = models.CharField(max_length=50)
     Car_BodyType = models.CharField(max_length=50)
     Car_FuelType = models.CharField(max_length=50)
     Car_Transmission = models.CharField(max_length=50)
-    Car_Milage = models.CharField(max_length=50)
+    Car_Milage = models.DecimalField(max_length=50)
 
     def __str__(self):
         return self.Car_Model
--- carviewer/views.py
+++ carviewer/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
-# Create your views here.
+def Brand(request):
+
+    Car_Brand = Car_Brand.objects.filter()
+    return render(request, 'carviewer/Brand.html',
+                  {'Brands': Brand})
+
+def Type(request):
+
+    Car_Type = Car_Type.objects.filter()
+    return render(request, 'carviewer/Type.html',
+                  {'Types': Type})
+
+def Model(request):
+
+    Car_Model = Car_Model.objects.filter()
+    return render(request, 'carviewer/Model.html',
+                  {'Models': Model})
