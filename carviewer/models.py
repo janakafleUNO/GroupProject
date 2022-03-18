503c496a9ad7250ee8b295905ea7878e28e5f430
janakafleUNO <jkafle@unomaha.edu>
2022-03-17 23:03:33 -0500

    Aded the New CarReview Model and fix some html error

--- carviewer/models.py
+++ carviewer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Define the model for the CarViewer table
 
 
@@ -13,7 +14,6 @@ class CarBrand(models.Model):
 
 
 class CarType(models.Model):
-
     car_brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE, related_name='cartypes', default=True)
     car_bodytype = models.CharField(max_length=50)
     car_fueltype = models.CharField(max_length=50)
@@ -27,14 +27,26 @@ class CarType(models.Model):
 
 class CarModel(models.Model):
     car_brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE, related_name='carmodels', default=True)
-    model_name = models.CharField(max_length=50)
     car_year = models.CharField(max_length=50)
-    car_price = models.DecimalField(max_digits=10, decimal_places=2)
+    model_name = models.CharField(max_length=50)
     car_bodytype = models.CharField(max_length=50)
     car_fueltype = models.CharField(max_length=50)
     car_transmission = models.CharField(max_length=50)
+    car_price = models.DecimalField(max_digits=10, decimal_places=2)
     car_mileage = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return str(self.car_brand)
 
+
+class CarReview(models.Model):
+    car_brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE, related_name='carreviews', default=True)
+    car_year = models.CharField(max_length=50)
+    model_name = models.CharField(max_length=50)
+    car_bodytype = models.CharField(max_length=50)
+    car_expert_review = models.CharField(max_length=100)
+    car_user_review = models.CharField(max_length=100)
+    car_overall_review = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.car_brand)
--- carviewer/urls.py
+++ carviewer/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^home/$', views.home, name='home'),
     path('carbrand_list', views.carbrand_list, name='carbrand_list'),
+    path('carbrand/create/', views.carbrand_new, name='carbrand_new'),
     path('carbrand/<int:pk>/edit/', views.carbrand_edit, name='carbrand_edit'),
     path('carbrand/<int:pk>/delete/', views.carbrand_delete, name='carbrand_delete'),
     path('cartype_list', views.cartype_list, name='cartype_list'),
@@ -18,5 +19,8 @@ urlpatterns = [
     path('carmodel/create/', views.carmodel_new, name='carmodel_new'),
     path('carmodel/<int:pk>/edit/', views.carmodel_edit, name='carmodel_edit'),
     path('carmodel/<int:pk>/delete/', views.carmodel_delete, name='carmodel_delete'),
-
+    path('carreview_list', views.carreview_list, name='carreview_list'),
+    path('carreview/create/', views.carreview_new, name='carreview_new'),
+    path('carreview/<int:pk>/edit/', views.carreview_edit, name='carreview_edit'),
+    path('carreview/<int:pk>/delete/', views.carreview_delete, name='carreview_delete'),
 ]
