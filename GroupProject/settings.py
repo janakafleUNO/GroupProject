cad7b6731782741113648990168dd5c3296768f3
janakafleUNO <jkafle@unomaha.edu>
2022-04-16 00:26:46 -0500

    Aded the code

--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'carviewer.apps.CarviewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'carviewer.apps.CarviewerConfig',
     'crispy_forms',
     'django.contrib.humanize',
 
